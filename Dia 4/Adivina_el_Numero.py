139db48a3c9d7df85fddb64e0129a4e6d2aaa10b
Nelson Gomez <n00gomez00@gmail.com>
2024-08-16 22:15:19 -0600

    add try catch validate

--- Dia 4/Adivina_el_Numero.py
+++ Dia 4/Adivina_el_Numero.py
@@ -1,6 +1,7 @@
 from random import randint
 
 intentos = 0
+intento = 0
 
 numero_secreto = randint(1, 100)
 
@@ -9,9 +10,15 @@ nombre = input("Hola! Cual es tu nombre? ")
 print(f"Bienvenido {nombre}! Estoy pensando en un numero entre 1 y 100\n Tienes 8 intentos para adivinar!")
 
 while intentos < 8:
-    intento = int(input("Cual es tu intento? "))
+    try:
+        intento = int(input("Cual es tu número? "))
+        if intento not in range(1, 101):
+            print("Por favor, introduce un numero entre 1 y 100")
+            continue
+    except ValueError:
+        print("Por favor, introduce un numero")
+        continue
     intentos += 1
-
     if intento < numero_secreto:
         print("Muy bajo! te quedan ", 8 - intentos, " intentos")
     elif intento > numero_secreto:
